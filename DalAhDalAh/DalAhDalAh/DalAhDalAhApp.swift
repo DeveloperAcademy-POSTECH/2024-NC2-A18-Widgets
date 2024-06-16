0bc98bf715bca8c8757949fe6624afececede326
Fine <92whdtjs@gmail.com>
2024-06-16 22:30:49 +0900

    init project

---
+++ DalAhDalAh/DalAhDalAh/DalAhDalAhApp.swift
@@ -0,0 +1,17 @@
+//
+//  DalAhDalAhApp.swift
+//  DalAhDalAh
+//
+//  Created by 이종선 on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DalAhDalAhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
